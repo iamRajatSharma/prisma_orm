a6a9a87c9aafbb6066b3a1db4bcb5f4a610f869f
Rajat Sharma <sharmarajat687@gmail.com>
2024-05-15 22:55:02 +0530

    set-up .env and migration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Express, Request, Response } from "express";
+import { PORT } from "./secrets";
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
     res.send({ messgae: "Hello" })
 })
 
-app.listen(3000, () => {
-    console.log('Server running')
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
