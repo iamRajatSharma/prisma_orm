3dd400e24048c5cd7bd282c4d490a2fd18293990
Rajat Sharma <sharmarajat687@gmail.com>
2024-05-16 23:27:03 +0530

    Create address model

---
+++ prisma/migrations/20240516175600_add_address_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE `addresses` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `line1` VARCHAR(191) NOT NULL,
+    `line2` VARCHAR(191) NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `country` VARCHAR(191) NOT NULL,
+    `pinceo` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `addresses` ADD CONSTRAINT `addresses_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express"
 import { prismaClient } from ".."
-import { Result } from "@prisma/client/runtime/library"
 
 export const createProduct = async (req: Request, res: Response) => {
 
