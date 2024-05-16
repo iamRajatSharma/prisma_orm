1ec88712ac54798f4aa796c211a8b534cb6bf359
Rajat Sharma <sharmarajat687@gmail.com>
2024-05-16 23:21:13 +0530

    Product-add, delete, update, fetch

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { prismaClient } from ".."
+import { Result } from "@prisma/client/runtime/library"
 
 export const createProduct = async (req: Request, res: Response) => {
 
@@ -10,4 +11,62 @@ export const createProduct = async (req: Request, res: Response) => {
         }
     })
     res.send(product)
+
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+    console.log('hello')
+    try {
+        const product = req.body
+        if (product.tags) {
+            product.tags = product.tags.join(",")
+        }
+
+        const updatedProduct = await prismaClient.product.update({
+            where: {
+                id: +req.params.id
+            },
+            data: product
+        })
+        res.send({ updatedProduct })
+    }
+    catch (err) {
+        res.send({ err })
+    }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    try {
+        const deleteProduct = await prismaClient.product.delete({ where: { id: +req.params.id } })
+        res.send({ deleteProduct })
+    }
+    catch (err) {
+        res.send({ err })
+    }
+}
+
+
+export const listProducts = async (req: Request, res: Response) => {
+    const count = await prismaClient.product.count()
+    const products = await prismaClient.product.findMany({
+        skip: 1,  // fixed for now
+        take: 2
+    })
+    res.send({ count, products })
+}
+
+
+export const getProductById = async (req: Request, res: Response) => {
+    try {
+        const product = await prismaClient.product.findFirst({
+            where: {
+                id: + req.params.id
+            }
+        })
+        res.send({ product })
+
+    }
+    catch (err) {
+        res.send({ err })
+    }
 }
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,10 +1,14 @@
 import { Router } from "express"
 import authMiddleware from "../middleware/auth";
-import { createProduct } from "../controllers/products";
+import { createProduct, deleteProduct, getProductById, listProducts, updateProduct } from "../controllers/products";
 import adminMiddleware from "../middleware/admin";
 
 const productsRoutes = Router();
 
 productsRoutes.post("/", [authMiddleware, adminMiddleware], createProduct)
+productsRoutes.put("/:id", [authMiddleware, adminMiddleware], updateProduct)
+productsRoutes.get("/", [authMiddleware, adminMiddleware], listProducts)
+productsRoutes.get("/:id", [authMiddleware, adminMiddleware], getProductById)
+productsRoutes.delete("/:id", [authMiddleware, adminMiddleware], deleteProduct)
 
 export default productsRoutes;
